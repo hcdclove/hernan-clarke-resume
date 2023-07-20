4e36404a9e6170fe831abe18f99e44062693a821
hcdclove <131356756+hcdclove@users.noreply.github.com>
2023-07-20 13:57:49 +0200

    My Resume version 5.0.0

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ app.set('view engine', 'ejs');
 
 // Route for index page
 app.get('/', (req, res) => {
-  res.render('index');
+	res.render('index');
 });
 
 app.post('/', (req, res) => {
-  const choice = req.body.submit;
-  // console.log('HI THERE: ' + choice);
-  if (choice === 'linkedin')
-    res.redirect('https://www.linkedin.com/in/hernan-clarke-0954ba273');
-  else res.redirect('https://hcportfolio.herokuapp.com/');
+	const choice = req.body.submit;
+	// console.log('HI THERE: ' + choice);
+	if (choice === 'linkedin')
+		res.redirect('https://www.linkedin.com/in/hernan-clarke-0954ba273');
+	else res.redirect('https://hcportfolio.herokuapp.com/');
 });
 
 app.listen(port, () => {
-  console.log('Listening on port ' + port);
+	console.log('Listening on port ' + port);
 });
