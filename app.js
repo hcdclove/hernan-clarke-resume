a98c02942fbe94eb461a1a5138945ec37b260105
hcdclove <131356756+hcdclove@users.noreply.github.com>
2023-07-29 07:05:44 +0200

    HC Resume version 7.2.1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
